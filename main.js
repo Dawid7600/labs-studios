a7af376087d0e6380881211a7dcc103d53689286
Dawid7600 <dawid7600@o2.pl>
2022-05-23 19:54:56 +0200

    Create a RWD

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ var swiper = new Swiper(".mySwiper", {
 	slidesPerView: "auto",
 	centeredSlides: true,
 	spaceBetween: 30,
+	breakpoints: {
+		1024: {
+			centeredSlides: false,
+			spaceBetween: 0,
+			slidesPerView: 4,
+			slidesPerGroup: 4,
+		},
+	},
 	pagination: {
 		el: ".swiper-pagination",
 		clickable: true,
